6224845d61df287c5ee2be9f61af9b614f358338
Anil <anil.antony@settlin.in>
2022-12-01 22:02:00 +0530

    fix: added changes

---
+++ Arrays/Richest_Customer_Wealth.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/richest-customer-wealth
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class Richest_Customer_Wealth {
+    public static void main(String[] args) {
+        int[][] array = {{1,2,5}, {4,2,1}};
+        // System.out.println(maximumWealth(array));
+        System.out.println(maximumWealth2(array));
+    }
+    public static int maximumWealth(int[][] accounts) {
+        int count = 0; 
+        for(int[] sum : accounts){
+            int temp = 0;
+            for(int i: sum){
+                temp += i;
+            }
+        count = Math.max(temp, count);
+        }
+        return count;
+}
+
+    public static int maximumWealth2(int[][] accounts) {
+        int count = 0; 
+        for(int[] account : accounts){
+        count = Math.max(Arrays.stream(account).sum(), count);
+        }
+        return count;
+    }
+
+}
