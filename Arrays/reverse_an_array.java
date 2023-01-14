68b6fdc34af61276310524e347b293b2c41e45e9
Anil Antony <anilantony3134@gmail.com>
2023-01-14 06:15:17 +0530

    feat: added dsa

--- Arrays/reverse_an_array.java
+++ Arrays/reverse_an_array.java
@@ -9,39 +9,11 @@ public class reverse_an_array {
         System.out.println(Arrays.toString(createReversedArray(nums, n)));
     }
     public static int[] createReversedArray(int[] nums, int n){
-        n = n % nums.length;
-        
+        for(int i=0; i<=(n-1)/2; i++){
+            int temp = nums[i];
+            nums[i] = nums[n-i-1];
+            nums[n-i-1] = temp;
+        }
+        return nums;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for(int i=0; i<=(n-1)/2; i++){
-//     int temp = nums[i];
-//     nums[i] = nums[n-i-1];
-//     nums[n-i-1] = temp;
-// }
-// return nums;
